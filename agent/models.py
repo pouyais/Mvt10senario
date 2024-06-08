6e9e1fc5527728b1e6f88bb1a09ddcb301bbbece
pouyais <157633426+pouyais@users.noreply.github.com>
2024-06-08 18:15:56 +0330

    ok

--- agent/models.py
+++ agent/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Agent(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='agent',default='default.jpg')
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    phone = models.IntegerField(default=0)
+
--- property/models.py
+++ property/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Propertie(models.Model):
+    image = models.ImageField(upload_to='property',default='notfound.jpg')
+    title = models.CharField(max_length=100)
+    rent = models.IntegerField(default=0)
+    area = models.IntegerField(default=0)
+    beds = models.IntegerField(default=0)
+    baths = models.IntegerField(default=0)
+    garages = models.IntegerField(default=0)
+
+
+    
