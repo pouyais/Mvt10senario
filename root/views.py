8607c7878c7dfa254d7fab41f60b3dc1b40cab56
pouyais <157633426+pouyais@users.noreply.github.com>
2024-06-09 02:59:01 +0330

    ok

--- root/views.py
+++ root/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+from .models import Service,Testimonial
+from property.models import Propertie
+from agent.models import Agent
+from blog.models import New
+
 
 
 
 
 def index(request):
-    return render(request,'root/index.html')
+
+    context = {
+        'services' : Service.objects.filter(status=True),
+        'properties' : Propertie.objects.filter(status=True),
+        'agents' : Agent.objects.filter(status=True),
+        'news' : New.objects.filter(status=True),
+        'testimonials' : Testimonial.objects.filter(status=True),
+    }
+
+    return render(request,'root/index.html',context=context)
 
 
 def about(request):
