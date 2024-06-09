1612e76c910b082c9ca97cef8c0f01733f7123a0
pouyais <157633426+pouyais@users.noreply.github.com>
2024-06-09 12:59:48 +0330

    ok

--- property/urls.py
+++ property/urls.py
@@ -6,5 +6,6 @@ app_name = 'property'
 
 urlpatterns = [
     path('',property_grid,name='property-grid'),
-    path('property-single/',property_single,name='property-single')
+    path('category/<str:category>',property_grid,name='list_by_category'),
+    path('property-single/',property_single,name='property-single'),
 ]
--- property/views.py
+++ property/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from .models import Propertie,Category_property
 
 
 
-def property_grid(request):
-    return render(request,'property/property-grid.html')
+def property_grid(request,**kwargs):
+
+    if kwargs.get('category'):
+        prop = Category_property.objects.filter(title=kwargs.get('category'))
+    else:
+        prop = Category_property.objects.all()
+
+    context = {
+        'category' : prop,
+        # 'category' : Category_property.objects.filter(status=True),
+    }
+
+    return render(request,'property/property-grid.html',context=context)
 
 
 def property_single(request):
